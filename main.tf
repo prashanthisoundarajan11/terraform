d67282bba387f22ef854a1692731654c2f08a5c9
Prashanthi Soundarajan <soundarajan@google.com>
2021-10-06 02:03:09 +0530

    added files

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "google_storage_bucket" "my-bucket" {
+  name          = "tf-toc-demo-17"
+  location      = "US"
+  project       = "gcp-scj3-ce-scj-dev-01"
+
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  credentials = file("/Users/soundarajan/Documents/test-diego-folder-59a90edcacd7.json")
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
