e84d8a6c6b0ebb82905b78ab45558350860ac633
Prashanthi Soundarajan <soundarajan@google.com>
2021-10-06 03:13:32 +0530

    added project

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name          = "tf-toc-demo-17"
   location      = "US"
-  project       = "gcp-scj3-ce-scj-dev-01"
+  project       = "pkhushalani-dev"
 
 }
