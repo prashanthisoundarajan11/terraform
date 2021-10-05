f6937453816c72f7248ac1feabb93e0ff6ac0604
Prashanthi Soundarajan <soundarajan@google.com>
2021-10-06 03:28:50 +0530

    changed bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "my-bucket" {
-  name          = "tf-toc-demo-17"
+zzzresource "google_storage_bucket" "my-bucket" {
+  name          = "tf-toc-demo-18"
   location      = "US"
   project       = "pkhushalani-dev"
 
