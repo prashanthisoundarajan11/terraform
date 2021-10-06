297017fff5717e929195d1aa4353693294491ce5
Prashanthi Soundarajan <soundarajan@google.com>
2021-10-06 09:13:35 +0530

    updated bucket name

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "tf-toc-demo-test"
+  name          = "tf-toc-demo-test-50"
   location      = "US"
   project       = "pkhushalani-dev"
 
