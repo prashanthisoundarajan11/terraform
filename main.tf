8526ffd4a17a5480ff0cf0d9bb31d961924dea2e
Prashanthi Soundarajan <soundarajan@google.com>
2021-10-06 12:42:16 +0530

    added bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "tf-toc-demo-40"
+  name          = "tf-toc-demo-jenkins"
   location      = "US"
   project       = "pkhushalani-dev"
 
